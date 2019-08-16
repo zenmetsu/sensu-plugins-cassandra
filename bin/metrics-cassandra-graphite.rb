4f0ae529a95e9fae84c3e5a479917d93ebcb2e99
zenmetsu <vespira@gmail.com>
2019-08-16 09:05:18 -0700

    Update metrics-cassandra-graphite.rb

--- bin/metrics-cassandra-graphite.rb
+++ bin/metrics-cassandra-graphite.rb
@@ -345,7 +345,7 @@ class CassandraMetrics < Sensu::Plugin::Metric::CLI::Graphite
         metric.gsub!(/[^a-zA-Z0-9]/, '_')  # convert all other chars to _
         metric.gsub!(/[_]*$/, '')          # remove any _'s at end of the string
         metric.gsub!(/[_]{2,}/, '_')       # convert sequence of multiple _'s to single _
-        metric.gsub!(/NaN/, '0')
+        metric.gsub!('NaN', '0')           # replacing NaN with zero due to potential graphite_plaintext issue
         metric.downcase!
         # sanitize metric values for graphite. Numbers only, please.
         # some versions of nodetool omit the '.' following the 'ms' unit.
